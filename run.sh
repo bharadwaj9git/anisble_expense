7f191dd5c35292686a3cb29005f712cb43880063
MYPC <bharadwajsai567@gmail.com>
2024-07-15 22:18:40 +0530

    pipeline

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+# template
+if [ "$pipeline" == "true"]; then
+  exit
+fi
+
+#
+
 ansible-playbook -i ${component}-${env}.saidevops79.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PWD} expense.yml -e role_name=${component} -e env=${env}
