cc2db82dc6c198b412a6536b2eab94abfd0d3ebd
MYPC <bharadwajsai567@gmail.com>
2024-07-15 22:39:35 +0530

    comp

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # template
-
-
+if ["$component" == "true"]; then
+  exit
+fi
 ##
 ansible-playbook -i ${component}-${env}.saidevops79.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PWD} expense.yml -e role_name=${component} -e env=${env}
