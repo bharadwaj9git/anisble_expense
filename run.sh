ff3f2bf5792382d2a879ae737bb55f10a6ea6776
MYPC <bharadwajsai567@gmail.com>
2024-08-26 20:17:48 +0530

    vault

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ fi
 
 
 ##
-ansible-playbook -i ${component}-${env}.saidevops79.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PWD}  -e role_name=${component} -e env=dev expense.yml
+ansible-playbook -i ${component}-${env}.saidevops79.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PWD}  -e role_name=${component} -e env=${{env}} -e vault_token=${vault_token} expense.yml
